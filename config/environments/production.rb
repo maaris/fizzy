7c79411b7d13ea72a271b033a91fc25408c76a2a
Maris Cilitis <maris.cilitis@gmail.com>
2025-12-06 22:50:42 +0200

    Add smtp config and deploy config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,15 +8,15 @@ Rails.application.configure do
   # Configure these according to whichever email provider you use. An example setup
   # using SMTP looks like the following:
   #
-  # config.action_mailer.smtp_settings = {
-  #   address:              'smtp.example.com', # The address of your email provider's SMTP server
-  #   port:                 2525,
-  #   domain:               'example.com',      # Your domain, which Fizzy will send email from
-  #   user_name:            ENV["SMTP_USERNAME"],
-  #   password:             ENV["SMTP_PASSWORD"],
-  #   authentication:       :plain,
-  #   enable_starttls_auto: true
-  # }
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.eu.mailgun.org', # The address of your email provider's SMTP server
+    port:                 2525,
+    domain:               'mg.webapps.lv',      # Your domain, which Fizzy will send email from
+    user_name:            ENV["SMTP_USERNAME"],
+    password:             ENV["SMTP_PASSWORD"],
+    authentication:       :plain,
+    enable_starttls_auto: true
+  }
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
